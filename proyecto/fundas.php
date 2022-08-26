05fad8809e4685f402255cec52609c7c00308d68
MichaelD002 <89713959+MichaelD002@users.noreply.github.com>
2022-08-26 18:48:37 -0500

    Add files via upload

---
+++ proyecto/fundas.php
@@ -0,0 +1,41 @@
+<?php include("template/header.php")?>
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="./img/descarga.png" alt="200" width="200"> 
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="./img/HUAWEI-LOGO.webp" alt="200" width="200">
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Text</p>
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="./img/Samsung-logo.webp" alt="200" width="200">
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Text</p>
+    </div>
+</div>
+</div>
+<div class="col-md-3">
+<div class="card">
+    <img class="card-img-top" src="./img/Xiaomi_logo_(2021-).svg.png" alt="200" width="200">
+    <a href="index.php"></a>
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Text</p>
+    </div>
+</div>
+</div>
+
+
+<?php include("template/pie.php")?>    
