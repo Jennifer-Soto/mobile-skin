9e3d08c28e8a4d92b2fa4eb57014d1f3bfc049f6
jennifer <jennifersp68@gmail.com>
2022-11-01 17:48:44 -0500

    Se agregan imagenes y se cambia bd

--- mobile-skin.sql
+++ mobile-skin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-09-2022 a las 15:19:18
+-- Tiempo de generación: 01-11-2022 a las 23:45:53
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
