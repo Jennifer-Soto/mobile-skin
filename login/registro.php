d6b3530030056caf470b48ff1426af6415207ac2
MichaelD002 <maicol.daza.1612@gmail.com>
2022-11-09 16:12:49 -0500

    insert creado

--- login/registro.php
+++ login/registro.php
@@ -1,26 +1,10 @@
-<?php 
-    /*Aqui incluimos la conexion*/
-    include("conexion.php");
-
-    /*Aqui traemos los datos pedidos en el formulario html*/
-        $nombre = $_POST['nombre'];
-        $usuario = $_POST['usuario'];
-        $contraseña = $_POST['password'];
-        
-    /*Insertamos los datos de las variables en el servidor*/
-    $insertar= "INSERT INTO usuario(nombre, usuarios, contraseña, tipo_usuario) values ('$nombre', '$usuario', '$contraseña', '2')";
-    if ($mysqli->query($insertar) === true){
-        echo "Excelente";
-    }
-    else {
-        echo "Pésimo trabajo";
-    }
-    echo "<br>";
-
+<?php
+$nombre="n";
+$usuarios="u";
+$contraseña="m";
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -37,7 +21,7 @@
     <link rel="stylesheet" href="../css/custom.css">
 </head>
 <body>
-<header class="header header_style_01">
+<header class="header header_style_01" >
         <nav class="navbar header-nav navbar-expand-lg">
             <div class="container">
 				<a class="navbar-brand" href=../index.php><img src="../images/logos/funda-mobile.svg" alt="" width="200px" height="100px"></a>
@@ -52,6 +36,7 @@
                         <li><a class="nav-link" href=""></a></li>
                         <li><a class="nav-link" href="formulario.php"><img src="images/carritoxd.png" alt="image" width="30px" height="30px"/></a></li>-->
                         <li><a class="nav-link active" href="../index.php"> <button class="btn btn-primary ">Salir</button>    </a></li>
+                        <li><a class="nav-link active" href="login.php"> <button class="btn btn-primary ">Iniciar</button>    </a></li>
 						<!--<li><a class="nav-link" href="#support">Support</a></li>-->
                     </ul>
                 </div>
@@ -60,22 +45,48 @@
     </header>
     <div id="home" class="parallax first-section"  style="background-image:url('../uploads/bg-img.jpg');  ">
         <!--<div style="display:flex; justify-content: center;">
-                <h1 style="color: white;  font-size:80px; " >Cree su cuenta:</h1>
+            <h1 style="color: white;  font-size:80px; " >Cree su cuenta:</h1>
         </div>-->
-    <div class=container>      
-    <form action="" method="post" class="form-style">
-        <label for="nombre" style="color: white" class="col-sm-2 control-label">Nombre: </label>
-        <input type="text" class="form-control" placeholder = "Escriba su nombre" name="nombre" ><br>
-        <label for="usuario" style="color: white" class="col-sm-2 control-label">Usuario: </label>
-        <input type="text" class="form-control" placeholder = "Escriba su usuario" name="usuario" ><br>
-        <label for="password" style="color: white" class="col-sm-2 control-label">Contraseña: </label>
-        <input type="password" class="form-control" placeholder = "Escriba su contraseña" name="password" ><br>
-        <label for="cpassword" style="color: white" class="col-sm-2 control-label">Conf. Contraseña: </label>
-        <input type="password" class="form-control" placeholder = "Confirme su contraseña" name="cpassword" ><br>
-        <div class="col-sm-offset-2 col-sm-2">
-            <button type="submit" class="btn btn-default">crear</button>
+        <div class=container>      
+            <div style="display:flex; justify-content: center;">
+                <h1 style="color: white;  font-size:80px; " >Registrate</h1>
+            </div>
+            <form action="registro.php" method="post" class="form-style">
+                <label for="nombre" style="color: white" class="col-sm-2 control-label">Nombre: </label>
+                <input type="text" class="form-control" placeholder = "Escriba su nombre" name="nombre" ><br>
+                <label for="usuario" style="color: white" class="col-sm-2 control-label">Usuario: </label>
+                <input type="text" class="form-control" placeholder = "Escriba su usuario" name="usuarios" ><br>
+                <label for="password" style="color: white" class="col-sm-2 control-label">Contraseña: </label>
+                <input type="password" class="form-control" placeholder = "Escriba su contraseña" name="password" ><br>
+                <div class="col-sm-offset-2 col-sm-2">
+                    <button type="submit" class="btn btn-default">crear</button>
+                </div>     
+            </form>
         </div>
-    </form>
-    </div>  
+  </div>  
 </body>
 </html>
+<?php
+/*Aqui incluimos la conexion*/
+    include("conexion.php");
+
+    /*Aqui traemos los datos pedidos en el formulario html*/
+        $nombre = $_POST['nombre'];
+        $usuarios = $_POST['usuarios'];
+        $contraseña = $_POST['password'];
+        
+    /*Insertamos los datos de las variables en el servidor*/
+    if($contraseña == "m"){
+
+    }else{
+    $insertar= "INSERT INTO usuario(nombre, usuarios, contraseña, tipo_usuario) values ('$nombre', '$usuarios', '$contraseña', '2')";
+    }
+   if ($mysqli->query($insertar) === true){
+        echo "Excelente";
+    }
+    else {
+        echo "Pésimo trabajo";
+    }
+    echo "<br>"; 
+
+?>
